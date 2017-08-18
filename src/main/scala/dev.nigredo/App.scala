c34a123b1732195ecfb0b0238c7bbfb0e0b35eb3
Andrii Ivanov <andrii.ivanov@citi.com>
2017-08-18 10:46:27 -0400

    Fix bugs

--- src/main/scala/dev.nigredo/App.scala
+++ src/main/scala/dev.nigredo/App.scala
@@ -6,6 +6,8 @@ import java.nio.file.{FileSystems, Files}
 import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
 import scala.meta._
+import scala.meta.parsers.Parsed.{Error, Success}
+import scala.util.Try
 
 object App {
 
@@ -20,13 +22,13 @@ object App {
         else failure(s"Path '$x' is not directory")
       }
       opt[String]('f', "loggerField").action((x, c) => c.copy(logger = x)).text("logger field name")
-      opt[String]('a', "annotation").action((x, c) => c.copy(annotation = x.replaceAllLiterally("{NL}", "\r\n")))
+      opt[String]('a', "annotation").action((x, c) => c.copy(annotation = x.replaceAllLiterally("_NL_", "\r\n")))
         .text("annotate with").required()
     }.parse(args, CommandLineArgs()) match {
       case Some(config) =>
         val path = new File(config.path)
-        if (path.isFile) showResult(run(List(path), config.annotation, config.logger))
-        else if (path.exists && path.isDirectory) showResult(run(findFiles(path), config.annotation, config.logger))
+        if (path.isFile) run(List(path), config.annotation, config.logger)
+        else if (path.exists && path.isDirectory) run(findFiles(path), config.annotation, config.logger)
       case None =>
     }
 
@@ -36,25 +38,23 @@ object App {
         .filter(Files.isRegularFile(_))
         .toList
         .map(_.toFile)
+        .filter(_.getPath.endsWith(".scala"))
     }
 
-    def showResult(data: List[(String, Int)]): Unit = {
-      println("*" * 50)
-      data.foreach(x => println(s"Process '${x._1}'. Changes: ${x._2}"))
-      println("*" * 50)
-    }
-
-    def run(files: List[File], annotation: String, loggerField: String): List[(String, Int)] = {
+    def run(files: List[File], annotation: String, loggerField: String): Unit = {
 
       def isLogField(name: Term): Boolean = name.toString == loggerField
 
       def isStrLit(t: Tree, p: Tree) = {
-        if (t.is[Term.Name])
-          p.collect {
+        t match {
+          case _: Term.Name => p.collect {
             case q"..$mods val ..$patsnel: $tpeopt = $expr" => patsnel.exists(t => expr.is[Lit.String])
             case _ => false
           }.nonEmpty
-        else t.is[Lit.String]
+          case t1: Term.ApplyInfix => t1.lhs.is[Lit.String]
+          case _: Lit.String => true
+          case _ => false
+        }
       }
 
       val annotated = ListBuffer.empty[Defn]
@@ -86,35 +86,46 @@ object App {
         rec(tree.parent)
       }
 
-      val processed = ListBuffer.empty[(String, Int)]
-
       files.foreach { file =>
-        val tree = file.parse[Source].get
-        var changes = 0
-        val annotated = tree.collect {
-          case t@q"$logField.$method($str)" if isLogField(logField) && isStrLit(str, tree) => getParentClassPos(t)
-          case t@q"$logField.$method($str, $cause)" if isLogField(logField) && str.is[Lit.String] => getParentClassPos(t)
-          case t@q"$logField.$method($str, ..$any)" if isLogField(logField) && str.is[Lit.String] => getParentClassPos(t)
-          case t@q"$logField.$method($marker, $str)" if isLogField(logField) => getParentClassPos(t)
-          case t@q"$logField.$method($marker, $str, $cause)" if isLogField(logField) => getParentClassPos(t)
-          case t@q"$logField.$method($marker, $str, ..$any)" if isLogField(logField) => getParentClassPos(t)
-        }.foldLeft(tree.tokens.tokens) {
-          case (zero, pos) =>
-            pos match {
-              case Position.None => zero
-              case _ =>
-                changes += 1
-                val indent = zero.filter(_.pos.start.line == pos.start.line).takeWhile(_.is[Token.Space])
-                val annot = annotation.tokenize.get.tokens.++:(indent)
-                zero.filter(_.pos.start.line < pos.start.line) ++ annot ++ zero.filter(_.pos.start.line >= pos.start.line)
-            }
+        Try(file.parse[Source]) match {
+          case scala.util.Success(source) => source match {
+            case Error(pos, msg, details) => sys.error(msg)
+            case Success(tree) =>
+              val changes = ListBuffer.empty[Int]
+              val tokens = tree.tokens.tokens
+              val annotated = tree.collect {
+                case t@q"$logField.$method($str)" if isLogField(logField) && isStrLit(str, tree) => getParentClassPos(t)
+                case t@q"$logField.$method($str, $cause)" if isLogField(logField) && str.is[Lit.String] => getParentClassPos(t)
+                case t@q"$logField.$method($str, ..$any)" if isLogField(logField) && str.is[Lit.String] => getParentClassPos(t)
+                case t@q"$logField.$method($marker, $str)" if isLogField(logField) => getParentClassPos(t)
+                case t@q"$logField.$method($marker, $str, $cause)" if isLogField(logField) => getParentClassPos(t)
+                case t@q"$logField.$method($marker, $str, ..$any)" if isLogField(logField) => getParentClassPos(t)
+              }.foldLeft(tokens) {
+                case (zero, pos) =>
+                  pos match {
+                    case Position.None => zero
+                    case _ =>
+                      changes += pos.start.line
+                      val indent = zero.filter(_.pos.start.line == pos.start.line).takeWhile(_.is[Token.Space])
+                      val annot = annotation.tokenize.get.tokens.++:(indent)
+                      zero.filter(_.pos.start.line < pos.start.line) ++ annot ++ zero.filter(_.pos.start.line >= pos.start.line)
+                  }
+              }
+              if (changes.nonEmpty) {
+                val bw = new BufferedWriter(new FileWriter(file))
+                bw.write(annotated.mkString)
+                bw.close()
+                println("*" * 50)
+                println(s"Annotate '${file.getPath}'. \r\n\t Lines: ${changes.mkString(",")}")
+                println("*" * 50)
+              }
+          }
+          case scala.util.Failure(exception) =>
+            println("*" * 50)
+            println(s"Could not anotate ${file.getPath} due to an error: \r\n\t${exception.getMessage}")
+            println("*" * 50)
         }
-        val bw = new BufferedWriter(new FileWriter(file))
-        bw.write(annotated.mkString)
-        bw.close()
-        processed.+=((file.getPath, changes))
       }
-      processed.toList
     }
   }
 }
